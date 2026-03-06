51db9e205e532d88c2fb0b527e1478b2a6a9c521
Frytoslav <filipbujakowski1@gmail.com>
2026-03-06 14:21:37 +0100

    feat: better Dashboard component layout and capitalizeFirstLetter function

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import type {FullWeatherData} from "../types/weather.ts";
-
+import capitalizeFirstLetter from "../utils/capitalizeFirstLetter.ts";
 
 function Dashboard({weatherData}: { weatherData: FullWeatherData | null }) {
     if (!weatherData) {
@@ -12,16 +12,19 @@ function Dashboard({weatherData}: { weatherData: FullWeatherData | null }) {
     }
     {/*TO DO: Change the styling to look more like a weather dashboard with icons and better layout*/}
     return (
-        <div className="w-full max-w-md mx-auto mt-10 bg-gray-950 bg-opacity-20 backdrop-blur-lg rounded-lg shadow-md p-6">
+        <div
+            className="w-full max-w-md mx-auto mt-10 bg-gray-950 bg-opacity-20 backdrop-blur-lg rounded-lg shadow-md p-6">
             <div className="flex items-center mb-4">
                 <h2 className="text-3xl text-white font-bold mr-4">{weatherData.header.city}, {weatherData.header.country}</h2>
                 <span className="text-sm text-gray-300">{weatherData.header.date}</span>
             </div>
-            <div className={`flex items-center mb-4`}>
-                <p className="text-sm text-gray-300">{weatherData.body.description}</p>
-                <p className="ml-4 text-gray-300">{weatherData.body.humidity}% Humidity</p>
-                <p className="text-sm text-gray-300">{weatherData.body.icon}</p>
-                <p className="font-bold">{weatherData.body.temperature}</p>
+            <div className={`flex items-center mb-4 w-full`}>
+                <div className="flex flex-col items-center w-full">
+                    <p className="text-4xl mb-4">{weatherData.body.icon}</p>
+                    <p className="text-sm text-gray-300 mb-2">{capitalizeFirstLetter(weatherData.body.description)}</p>
+                    <p className="text-sm text-gray-300 mb-2">{weatherData.body.humidity}% Humidity</p>
+                    <p className="text-2xl font-bold text-white">{weatherData.body.temperature}</p>
+                </div>
             </div>
         </div>
     );
