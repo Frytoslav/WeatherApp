65f5a1cab38b6feb047f048e7b90476538e6e51a
Frytoslav <filipbujakowski1@gmail.com>
2026-03-06 22:20:20 +0100

    feat: enhance dynamic background handling with weather-specific styles

--- src/hooks/useDynamicBackground.ts
+++ src/hooks/useDynamicBackground.ts
@@ -1,10 +1,25 @@
 function useDynamicBackground (weatherId: number | null | undefined) {
     if (!weatherId) return "";
 
-    if (weatherId == 800) {
-        return "with-sun-rays";
+    switch (true) {
+        case (weatherId >= 200 && weatherId < 600):
+            return "with-rain";
+
+        case (weatherId >= 600 && weatherId < 700):
+            return "with-snow";
+
+        case (weatherId >= 700 && weatherId < 800):
+            return "with-fog";
+
+        case (weatherId === 800):
+            return "with-sun-rays";
+
+        case (weatherId > 800 && weatherId < 900):
+            return "with-clouds";
+
+        default:
+            return "";
     }
-    return "";
 }
 
 export { useDynamicBackground };
